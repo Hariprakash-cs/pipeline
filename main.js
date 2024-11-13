19aeb24c9c929cc389497309c94c8d2da742d2cb
Hari Prakash <hari.prakash@contentstack.com>
2024-11-13 17:48:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 // Function to generate a random number and log it
 function getRandomNumber() {
     const randomNumber = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
-    console.log('Generated Random Number:', randomNumber);
     return randomNumber;
 }
 
-// Call the function and use the
 const randomNumber = getRandomNumber();
 console.log('Random Number:', randomNumber);
