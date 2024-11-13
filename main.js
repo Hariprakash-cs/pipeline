a2a649329d3b0988e9a2323b44ccd9764f483d1e
Hari Prakash <hari.prakash@contentstack.com>
2024-11-13 17:28:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getRandomNumber() {
     const randomNumber = Math.floor(Math.random() * 100) + 1; 
-    console.log("Generated Random Number:", randomNumber);
+       console.log("Generated Random Number:", randomNumber);
 }
 getRandomNumber();
